c67959ab6dd784db67ac65b8fafbec84a8542737
Yogesh Singh <ysingh@gainsight.com>
2017-04-16 14:53:45 +0530

    print array

--- src/UpdateArray.java
+++ src/UpdateArray.java
@@ -5,11 +5,14 @@ public class UpdateArray {
     public static void main(String[] args) {
         int [] arr = {1, 2, 3, 4, 5, 6};
         updateArray(arr, arr.length - 1);
+        printArray(arr, arr.length - 1);
+    }
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+    private static void printArray(int[] arr, int n) {
+        if(n > 0) {
+            printArray(arr, n - 1);
+            System.out.print(arr[n] + " ");
         }
-
     }
 
     private static int updateArray(int[] arr, int n) {
