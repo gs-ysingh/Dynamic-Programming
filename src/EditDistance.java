fd1bef73a3ea1b4c8f7a8abe3367580721294b84
Yogesh Singh <ysingh@gainsight.com>
2017-04-30 00:11:32 +0530

    Updated base condition

--- src/EditDistance.java
+++ src/EditDistance.java
@@ -3,22 +3,39 @@
  */
 public class EditDistance {
     public static void main(String[] args) {
-        String str1 = "CAT";
-        String str2 = "CAR";
+        String str1 = "sunday";
+        String str2 = "saturday";
         
         System.out.println(getDistance(str1, str2));
     }
 
     private static int getDistance(String str1, String str2) {
-        int [][] M = new int[str1.length() + 1][str2.length() + 1];
-        for (int i = 1; i < str1.length(); i++) {
-            for (int j = 1; j < str2.length(); j++) {
-                if(str1.charAt(i) == str2.charAt(j)) {
-                    M[i][j] = 0;
+        int row = str1.length();
+        int col = str2.length();
+        int [][] M = new int[row + 1][col + 1];
+
+        for (int i = 1; i <= row; i++) {
+            M[i][0] = i;
+        }
+        for (int j = 1; j <= col; j++) {
+            M[0][j] = j;
+        }
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if(str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    M[i][j] = M[i - 1][j - 1];
+                }
+                else {
+                    M[i][j] = 1 + getMin(M[i - 1][j], M[i - 1][j - 1], M[i][j - 1]);
                 }
             }
         }
 
-        return M[str1.length()][str2.length()];
+        return M[row][col];
+    }
+
+    private static int getMin(int a, int b, int c) {
+        return Math.min(Math.min(a, b), c);
     }
 }
