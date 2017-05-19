0ab90337d1b6629faf4f8127adcf50bb4be0a874
Yogesh Singh <ysingh@gainsight.com>
2017-05-20 00:35:26 +0530

    matrix multiplication using recursion

--- src/MatrixChainMultiplication.java
+++ src/MatrixChainMultiplication.java
@@ -4,10 +4,29 @@
 public class MatrixChainMultiplication {
     public static void main(String[] args) {
         int [] arr = {1, 2, 3, 4};
-        System.out.println(minMultiplication(arr, arr.length));
+        //System.out.println(minMultiplicationDP(arr, arr.length));
+        System.out.println(minMultiplication(arr, 1, arr.length - 1));
     }
 
-    private static int minMultiplication(int[] arr, int n) {
+    private static int minMultiplication(int[] arr, int start, int end) {
+        if(start == end) {
+            return 0;
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for (int k = start; k < end; k++) {
+            min = Math.min(min,
+                    minMultiplication(arr, start, k)
+                            + minMultiplication(arr, k + 1, end)
+                            + arr[start - 1] * arr[k] * arr[end]
+            );
+        }
+
+        return min;
+    }
+
+    private static int minMultiplicationDP(int[] arr, int n) {
         int [][] M = new int[n][n];
 
         for (int i = 1; i < n; i++) {
